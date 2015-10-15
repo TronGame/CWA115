d11c04dedb6e15e342a0b231d93dbe3240fc7a9d
PeterCoppens <petercoppens@skynet.be>
2015-10-15 15:09:42 +0200

    Added Map package and moved Map class

--- app/src/main/java/cwa115/trongame/GameActivity.java
+++ app/src/main/java/cwa115/trongame/GameActivity.java
@@ -1,20 +1,15 @@
 package cwa115.trongame;
 
-import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import cwa115.trongame.Map.Map;
 
 public class GameActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
@@ -60,7 +55,6 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onLocationChanged(Location location) {
-
 
     }
 
