faf2110007ec827b87c2780ed7c912f3d14e311f
PeterCoppens <petercoppens@skynet.be>
2015-10-15 15:01:59 +0200

    Added comments and Map.setMap function

--- app/src/main/java/cwa115/trongame/GameActivity.java
+++ app/src/main/java/cwa115/trongame/GameActivity.java
@@ -17,22 +17,32 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class GameActivity extends AppCompatActivity
     implements OnMapReadyCallback {
 
+    // The map storage object. This stores all the item and player information on the map
+    private Map map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the view
         setContentView(R.layout.activity_game);
 
+        // Initialize the map storage object
+        map = new Map();
+
+        // Activate the map fragment defined in the content_game.xml layout
         MapFragment mapFragment =
                 (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * Activates when the google map object has started up and is ready to be controlled
+     */
     @Override
-    public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
-
+    public void onMapReady(GoogleMap map_fragment) {
+        // Store the GoogleMap object in the map storage object.
+        map.setMap(map_fragment);
     }
 
 }
