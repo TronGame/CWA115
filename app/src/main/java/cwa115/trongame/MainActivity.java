7d73a6a6a1727ab42e97cda851c25659e885bd5d
Bram <bram.decooman@gmail.com>
2015-11-09 17:44:06 +0100

    Very poor facebook support

--- app/src/main/java/cwa115/trongame/MainActivity.java
+++ app/src/main/java/cwa115/trongame/MainActivity.java
@@ -3,14 +3,27 @@ package cwa115.trongame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import cwa115.trongame.Test.SensorData;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +34,47 @@ public class MainActivity extends AppCompatActivity {
         // Initialize SensorData
         //SensorData.Initialize(this);
         //SensorData.Test(this); // Only for testing purposes
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Initialize the SDK before executing any other operations,
+        // especially, if you're using Facebook UI elements.
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+        ((TextView) findViewById(R.id.facebook_token)).setText(AccessToken.getCurrentAccessToken().getUserId());
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("FACEBOOK_LOGIN","Login succeeded: " + loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("FACEBOOK_LOGIN","Login canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("FACEBOOK_LOGIN","Login error: " + error.toString());
+            }
+        });
     }
 
     @Override
     protected void onPause(){
         super.onPause();
         //SensorData.Pause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
         //SensorData.Resume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
     }
 
     @Override
@@ -55,6 +97,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void showGameActivity(View view) {
