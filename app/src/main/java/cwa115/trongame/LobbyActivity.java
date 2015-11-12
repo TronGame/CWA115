9c66ea4c057bc893c5676063f1e9ed291bb9d99f
Valentin-69 <valentin@cvbhome.be>
2015-11-12 14:41:20 +0100

    added yuchengs code to lobbyActivity

--- app/src/main/java/cwa115/trongame/LobbyActivity.java
+++ app/src/main/java/cwa115/trongame/LobbyActivity.java
@@ -2,13 +2,93 @@ package cwa115.trongame;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import java.net.URL;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 
 public class LobbyActivity extends AppCompatActivity {
+
+    String dataToPut;
+
+
+    public String getDataFromServer(String URL){
+        String result="";
+        try {
+            URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            while ((output = br.readLine()) != null) {
+                result += output;
+            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+        return result;
+    };
+
+    /*****************************can be reused in your app*******************************
+     * a method to update or add data to the server
+     * @param URL
+     * @return
+     */
+
+    public String putDataToServer(String URL){
+        String status="Put the data to server successfully!";
+        try {
+
+            URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("PUT");
+
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            //conn.setRequestProperty("Content-Type", "application/json");
+
+            String input = dataToPut;
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+
+            //Read the acknowledgement message after putting data to server
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
