20319f0c3f5df1df1dc4db0c700b19c096682f0f
Tim Beyne <tim_beyne@hotmail.com>
2015-11-23 18:01:48 +0100

    Uncomment startGame(); line

--- app/src/main/java/cwa115/trongame/RoomActivity.java
+++ app/src/main/java/cwa115/trongame/RoomActivity.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -133,7 +131,7 @@ public class RoomActivity extends AppCompatActivity {
                             GameSettings.setWallColor(listOfColors.get(i));
                     }
                     GameSettings.setPlayersInGame(listOfPlayerIds);
-                    // startGame();
+                    startGame();
                     showGameActivity();
 
                 } catch (JSONException e) {
