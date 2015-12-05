4c1a96ed093446d3ebe284a290aeb8444ee4dcc1
Bram <bram.decooman@gmail.com>
2015-12-05 11:49:43 +0100

    Implemented Service part 3 (Basics is working now) + bug fix (logical error) in Friend class

--- app/src/main/java/cwa115/trongame/Service/AppReceiver.java
+++ app/src/main/java/cwa115/trongame/Service/AppReceiver.java
@@ -6,14 +6,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 
 /**
  * Code inspiration from http://it-ride.blogspot.be/2010/10/android-implementing-notification.html
  */
 public class AppReceiver extends BroadcastReceiver {
 
-    private final static int REPEAT_MINUTES = 5;
+    private final static int REPEAT_MINUTES = 1;
+    private final static String TAG = "AppReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         // in our case intent will always be CONNECTIVITY_ACTION, so we can just set
@@ -21,11 +26,15 @@ public class AppReceiver extends BroadcastReceiver {
         // Note that a BroadcastReceiver is *NOT* a Context. Thus, we can't use
         // "this" whenever we need to pass a reference to the current context.
         // Thankfully, Android will supply a valid Context as the first parameter
+        Log.d(TAG, "intent received");
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, NotificationService.class);
         PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
         am.cancel(pi);
-        if(!intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY,true)) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if(activeNetwork!=null && activeNetwork.isConnected()) {
+            Log.d(TAG, "Connected to internet => setRepeating service call");
             am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                     SystemClock.elapsedRealtime() + REPEAT_MINUTES * 60 * 1000,
                     REPEAT_MINUTES * 60 * 1000, pi);
