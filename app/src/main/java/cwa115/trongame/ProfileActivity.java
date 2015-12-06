82068377e67292e53a16d58304d763c517101193
Bram <bram.decooman@gmail.com>
2015-12-06 23:28:58 +0100

    Friends implementation part 4 (only thing to do now is reload profile data in OnResume)

--- app/src/main/java/cwa115/trongame/ProfileActivity.java
+++ app/src/main/java/cwa115/trongame/ProfileActivity.java
@@ -11,6 +11,12 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +25,8 @@ import cwa115.trongame.Game.GameSettings;
 import cwa115.trongame.Lists.StatsCustomAdapter;
 import cwa115.trongame.Lists.StatsListItem;
 import cwa115.trongame.Network.Server.HttpConnector;
+import cwa115.trongame.Network.Server.ServerCommand;
+import cwa115.trongame.User.Friend;
 import cwa115.trongame.User.Profile;
 import cwa115.trongame.Utils.DrawableManager;
 
@@ -26,18 +34,26 @@ public class ProfileActivity extends AppCompatActivity {
 
     public final static String DATA_EXTRA = "profileActivity_dataExtra";
     public final static String PROFILE_EXTRA = "profileActivity_profileExtra";
+    public final static String DELETE_ACCOUNT_EXTRA = "profileActivity_deleteAccountExtra";
 
-    private final static int FRIEND_LIST_REQUEST_CODE = 2;
+    private final static int VIEW_PENDING_FRIEND = 0;
+    private final static int VIEW_FRIEND_OR_STRANGER = 1;
+    private final static int VIEW_OWN_PROFILE = 2;
+    private final static int OWN_PROFILE_STATE = 0;
+    private final static int STRANGER_PROFILE_STATE = 1;
+    private final static int FRIEND_PROFILE_STATE = 2;
+    private final static int PENDING_FRIEND_INVITER_STATE = 3;
+    private final static int PENDING_FRIEND_INVITEE_STATE = 4;
 
     private HttpConnector dataServer;
     private Profile profile;
     private List<StatsListItem> statsList;
     private StatsCustomAdapter statsCustomAdapter;
+    private int currentState;
 
     private ImageView profileImageView, facebookFlag;
     private TextView usernameTextView;
-    private LinearLayout friendFooter;
-    private Button friendListButton;
+    private ViewFlipper footerFlipper;
     private ListView statsListView;
 
     @Override
@@ -58,8 +74,7 @@ public class ProfileActivity extends AppCompatActivity {
         profileImageView = (ImageView)findViewById(R.id.profileImageView);
         facebookFlag = (ImageView)findViewById(R.id.facebookFlag);
         usernameTextView = (TextView)findViewById(R.id.userNameTextView);
-        friendFooter = (LinearLayout)findViewById(R.id.friendFooter);
-        friendListButton = (Button)findViewById(R.id.friendListButton);
+        footerFlipper = (ViewFlipper)findViewById(R.id.footer_flipper);
         statsListView = (ListView)findViewById(R.id.statsListView);
 
         loadProfile();
@@ -92,22 +107,23 @@ public class ProfileActivity extends AppCompatActivity {
         }else
             usernameTextView.setText(profile.getName());
 
-        if(profile.getId()!=null && profile.getToken()!=null) { // Assume a valid id and token is set
-            friendFooter.setVisibility(View.GONE);// So user views his own profile
-            friendListButton.setVisibility(View.VISIBLE);
-        }else {
-            friendFooter.setVisibility(View.VISIBLE);
-            friendListButton.setVisibility(View.GONE);
-            if(GameSettings.getFriends()!=null && GameSettings.getFriends().ToIdList().contains((long)profile.getId())){
-                // Player is a friend
-                ((TextView)findViewById(R.id.friend_text)).setText(String.format(getString(R.string.friends_message),""));
-                ((Button)findViewById(R.id.friend_button)).setText(R.string.delete_friend);
-            }else{
-                // Player is not a friend
-                ((TextView)findViewById(R.id.friend_text)).setText(String.format(getString(R.string.friends_message),"not"));
-                ((Button)findViewById(R.id.friend_button)).setText(R.string.add_friend);
-            }
-        }
+        if(profile.getId()!=null && profile.getToken()!=null) // Assume a valid id and token is set
+            currentState = OWN_PROFILE_STATE;// So user views his own profile
+        else if(GameSettings.getFriends()!=null && GameSettings.getFriends().ToIdList().contains((long)profile.getId())){
+            Friend friend = GameSettings.getFriends().get(GameSettings.getFriends().ToIdList().indexOf((long)profile.getId()));
+            if(friend.isPending()){
+                if(!friend.isInviter()){
+                    currentState = PENDING_FRIEND_INVITEE_STATE;// Current user is inviter
+                }else{
+                    currentState = PENDING_FRIEND_INVITER_STATE;// Current user is invited
+                }
+            }else
+                currentState = FRIEND_PROFILE_STATE;// Player is a friend
+        }else
+            currentState = STRANGER_PROFILE_STATE;// Player is not a friend
+
+        updateFooterFlipper();
+
         if(profile.getPictureUrl()==null)
             profileImageView.setImageResource(R.mipmap.default_profile_picture);
         else
@@ -119,6 +135,35 @@ public class ProfileActivity extends AppCompatActivity {
             facebookFlag.setVisibility(View.VISIBLE);
 
         loadStats();
+    }
+
+    private void updateFooterFlipper(){
+        switch(currentState){
+            case OWN_PROFILE_STATE:
+                footerFlipper.setDisplayedChild(VIEW_OWN_PROFILE);
+                break;
+            case STRANGER_PROFILE_STATE:
+                footerFlipper.setDisplayedChild(VIEW_FRIEND_OR_STRANGER);
+                ((TextView) findViewById(R.id.friend_text)).setText(R.string.not_friends_message);
+                ((Button) findViewById(R.id.friend_button)).setText(R.string.add_friend);
+                break;
+            case FRIEND_PROFILE_STATE:
+                footerFlipper.setDisplayedChild(VIEW_FRIEND_OR_STRANGER);
+                ((TextView) findViewById(R.id.friend_text)).setText(R.string.friends_message);
+                ((Button) findViewById(R.id.friend_button)).setText(R.string.delete_friend);
+                break;
+            case PENDING_FRIEND_INVITEE_STATE:
+                // Current user is inviter, so he can only reject the friend request
+                findViewById(R.id.accept_friend_request).setVisibility(View.GONE);
+                ((TextView)findViewById(R.id.friend_pending_text)).setText(R.string.friends_pending_invitee_message);
+                footerFlipper.setDisplayedChild(VIEW_PENDING_FRIEND);// Player is a pending friend
+                break;
+            case PENDING_FRIEND_INVITER_STATE:
+                // Current user is invited
+                ((TextView)findViewById(R.id.friend_pending_text)).setText(R.string.friends_pending_inviter_message);
+                footerFlipper.setDisplayedChild(VIEW_PENDING_FRIEND);// Player is a pending friend
+                break;
+        }
     }
 
     private void loadStats(){
@@ -145,22 +190,127 @@ public class ProfileActivity extends AppCompatActivity {
         Bundle data = new Bundle();
         data.putString(FriendsListActivity.TITLE_EXTRA, "Friend List");
         data.putParcelable(FriendsListActivity.PROFILE_EXTRA, profile);
-        //data.putBoolean(FriendsListActivity.SELECTABLE_EXTRA, true);
 
         Intent intent = new Intent(this, FriendsListActivity.class);
         intent.putExtra(FriendsListActivity.DATA_EXTRA, data);
 
-        //startActivityForResult(intent, FRIEND_LIST_REQUEST_CODE);
         startActivity(intent);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FRIEND_LIST_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                int[] selectedIds = data.getIntArrayExtra(FriendsListActivity.SELECTED_IDS_EXTRA);
-            }
+    public void deleteAccount(View v){
+        Intent data = new Intent();
+        data.putExtra(DELETE_ACCOUNT_EXTRA, true);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    public void acceptFriendRequest(View v) {
+        dataServer.sendRequest(
+                ServerCommand.ACCEPT_FRIEND,
+                ImmutableMap.of(
+                        "id", GameSettings.getPlayerId(),
+                        "token", GameSettings.getPlayerToken(),
+                        "friendId", String.valueOf(profile.getId())
+                ),
+                new HttpConnector.Callback() {
+                    @Override
+                    public void handleResult(String data) {
+                        try {
+                            JSONObject result = new JSONObject(data);
+                            if (!result.has("error") && result.getBoolean("success")) {
+                                showToast("Friend request accepted!");
+                                currentState = FRIEND_PROFILE_STATE;
+                                updateFooterFlipper();
+                            } else
+                                showToast("Error while trying to accept friend request.");
+                        } catch (JSONException e) {
+                            showToast("Error while accepting friend request.");
+                        }
+                    }
+                }
+        );
+    }
+
+    public void rejectFriendRequest(View v){
+        dataServer.sendRequest(
+                ServerCommand.DELETE_FRIEND,
+                ImmutableMap.of(
+                        "id", GameSettings.getPlayerId(),
+                        "token", GameSettings.getPlayerToken(),
+                        "friendId", String.valueOf(profile.getId())
+                ),
+                new HttpConnector.Callback() {
+                    @Override
+                    public void handleResult(String data) {
+                        try{
+                            JSONObject result = new JSONObject(data);
+                            if(!result.has("error") && result.getBoolean("success")) {
+                                showToast("Friend request rejected!");
+                                currentState = STRANGER_PROFILE_STATE;
+                                updateFooterFlipper();
+                            }else
+                                showToast("Error while trying to reject friend request.");
+                        }catch (JSONException e){
+                            showToast("Error while rejecting friend request.");
+                        }
+                    }
+                }
+        );
+    }
+
+    public void friendButtonPressed(View v){
+        if(currentState==STRANGER_PROFILE_STATE){
+            // Add as friend
+            dataServer.sendRequest(
+                    ServerCommand.ADD_FRIENDS,
+                    ImmutableMap.of(
+                            "id", GameSettings.getPlayerId(),
+                            "token", GameSettings.getPlayerToken(),
+                            "friends", "[" + String.valueOf(profile.getId()) + "]"
+                    ),
+                    new HttpConnector.Callback() {
+                        @Override
+                        public void handleResult(String data) {
+                            try {
+                                JSONObject result = new JSONObject(data);
+                                if (!result.has("error") && result.getBoolean("success")) {
+                                    showToast("Friend request sent!");
+                                    currentState = PENDING_FRIEND_INVITEE_STATE;
+                                    updateFooterFlipper();
+                                } else
+                                    showToast("Error while trying to send friend request.");
+                            } catch (JSONException e) {
+                                showToast("Error while sending friend request.");
+                            }
+                        }
+                    }
+            );
+        }else if(currentState==FRIEND_PROFILE_STATE){
+            // Delete friend
+            dataServer.sendRequest(
+                    ServerCommand.DELETE_FRIEND,
+                    ImmutableMap.of(
+                            "id", GameSettings.getPlayerId(),
+                            "token", GameSettings.getPlayerToken(),
+                            "friendId", String.valueOf(profile.getId())
+                    ),
+                    new HttpConnector.Callback() {
+                        @Override
+                        public void handleResult(String data) {
+                            try{
+                                JSONObject result = new JSONObject(data);
+                                if(!result.has("error") && result.getBoolean("success")) {
+                                    showToast("Friend deleted!");
+                                    currentState = STRANGER_PROFILE_STATE;
+                                    updateFooterFlipper();
+                                }else
+                                    showToast("Error while trying to delete friend.");
+                            }catch (JSONException e){
+                                showToast("Error while deleting friend.");
+                            }
+                        }
+                    }
+            );
         }
     }
 
